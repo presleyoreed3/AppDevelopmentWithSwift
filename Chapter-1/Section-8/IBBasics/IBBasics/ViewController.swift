d442ab6c2fd5bb95aed562f6160745fc57becd1b
presleyoreed3 <presleyoreed3@gmail.com>
2017-07-10 12:55:24 -0700

    added action the the button to have text display to the connsole

--- Chapter-1/Section-8/IBBasics/IBBasics/ViewController.swift
+++ Chapter-1/Section-8/IBBasics/IBBasics/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
         myButton.setTitleColor(.red, for: .normal)
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+    print("The button was pressed")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
