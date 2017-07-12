7eff245070a0a126ed6b1315abd5d7402b670016
presleyoreed3 <presleyoreed3@gmail.com>
2017-07-12 16:11:51 -0700

    began workig on ch2 section 2 lab

--- Textbook Resources/AppDevelopmentSwift-Student/2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ Textbook Resources/AppDevelopmentSwift-Student/2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -3,12 +3,25 @@
  
  Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the funtion and observe the printout.
  */
-
+func introduceMyself(firstName: String, lastName: String){
+    print ("Hello my names is \(firstName) \(lastName)")
+}
+introduceMyself(firstName: "Presley", lastName: "Reed III")
 
 /*:
  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = arc4random_uniform(UInt32(5))` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts. Note that the random number generation will not work unless your function is declared after the `import Foundation` statement below. This is because the `arc4random_uniform(_:)` function is declared in the `Foundation` framework.
  */
 import Foundation
 
+func magicEightBall(randomNum: UInt32){
+        switch randomNum {
+    case 0,2,4:
+        print("You got an even number!")
+    default:
+        print("You got an odd number!")
+    }
+}
+
+magicEightBall(randomNum: arc4random_uniform(UInt32(5)))
 
 //: page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
