ab2f650b50fcf5e56a626b7e8ad64dcd7b7c356d
presleyoreed3 <presleyoreed3@gmail.com>
2017-07-12 16:35:59 -0700

    due to uncorrectable error, had to skip section 2 of the ch2 section 2 lab

--- Textbook Resources/AppDevelopmentSwift-Student/2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
+++ Textbook Resources/AppDevelopmentSwift-Student/2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
@@ -1,4 +1,4 @@
-/*:
+t/*:
  ## App Exercise - A Functioning App
  
  >These exercises reinforce Swift concepts in the context of a fitness tracking app.
@@ -7,8 +7,6 @@
  
  A reoccurring process like this is a perfect candidate for a function. Write a function called `incrementSteps` after the declaration of `steps` below that will increment `steps` by one and then print its value. Call the function multiple times and observe the printouts.
  */
-var steps = 0
-
 
 /*:
  Similarly, if you want to regularly provide progress updates to your user, you can put your control flow statements that check on progress into a function. Write a function called `progressUpdate` after the declaration of `goal` below. The function should print "You're off to a good start." if `steps` is less than 10% of `goal`, "You're almost halfway there!" if `steps` is less than half of `goal`, "You're over halfway there!" if `steps` is less than 90% of `goal`, "You're almost there!" if `steps` is less than `goal`, and "You beat your goal!" otherwise. Call the function and observe the printout. Remember, you can convert numbers using the appropriate Int or Double initializer.
