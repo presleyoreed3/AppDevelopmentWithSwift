17c886a6b565817a81d191271fa6fa442e4a6387
presleyoreed3 <presleyoreed3@gmail.com>
2017-07-12 12:26:30 -0700

    retroactively completed the lab for Ch2 section 1.

--- Textbook Resources/AppDevelopmentSwift-Student/2 - Introduction to UIKit/1 - Strings/lab/Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
+++ Textbook Resources/AppDevelopmentSwift-Student/2 - Introduction to UIKit/1 - Strings/lab/Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
@@ -3,7 +3,7 @@
  
  Create a `name` constant and assign it a string literal representing your name.
  */
-
+    let myName = "Presley Orelle Reed III"
 
 /*:
  Create a `favoriteQuote` constant and assign it the following string literal:
@@ -12,12 +12,18 @@
  
  Write in your own favorite quote where indicated, and be sure to include escaped quotation marks. Print the value of `favoriteQuote`
  */
-
+    let myFaveQuote = "\"To see the world, things dangerous to come to, to see behind walls, draw closer, to find each other, and to feel. That is the purpose of Life.\""
+    print ("My favoirite quote is \(myFaveQuote)")
 
 /*:
  Write an if-else statement that prints "There's nothing here" if `emptyString` is empty, and "It's not as empty as I thought" otherwise.
  */
-let emptyString = ""
-
+    let emptyString = ""
+if (emptyString == ""){
+    print ("There's nothing here")
+}
+else {
+    print ("It's not as empty ad I thought")
+}
 
 //: page 1 of 5  |  [Next: Exercise - Concatenation and Interpolation](@next)
--- Textbook Resources/AppDevelopmentSwift-Student/2 - Introduction to UIKit/1 - Strings/lab/Lab - Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
+++ Textbook Resources/AppDevelopmentSwift-Student/2 - Introduction to UIKit/1 - Strings/lab/Lab - Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
@@ -3,12 +3,18 @@
  
  Create a `city` constant and assign it a string literal representing your home city. Then create a `state` constant and assign it a string literal representing your home state. Finally, create a `home` constant and use string concatenation to assign it a string representing your home city and state (i.e. Portland, Oregon). Print the value of `home`.
  */
+let city = "Boulder"
+let state = "Colorado"
+let home = city + ", " + state
 
+print (home)
 
 /*:
  Use the compound assignment operator (`+=`) to add `home` to `introduction` below. Print the value of `introduction`.
  */
-var introduction = "I live in"
+var introduction = "I live in "
+introduction += home
+print (introduction)
 
 
 /*:
@@ -18,6 +24,8 @@ var introduction = "I live in"
  
  Insert `name` where indicated, and insert a mathematical expression that evaluates to your current age plus one where indicated.
  */
-
+let name = "Presley Orelle Reed III"
+var age = 21
+print ("My name is \(name) and after my next birthday I will be \(1+age)")
 
 //: [Previous](@previous)  |  page 2 of 5  |  [Next: App Exercise - Notifications](@next)
